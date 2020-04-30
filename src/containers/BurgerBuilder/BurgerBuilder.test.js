0ceb5e3c6abb3a62f7b4f972e7b40ee82870e07f
mfradcourt <mfradcourt@gmail.com>
2020-04-30 18:40:27 +0200

    Adding my first form !

--- src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { shallow } from "enzyme";
 
 import { BurgerBuilder } from './BurgerBuilder';
+import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 
 describe('<BurgerBuilder />', () => {
   let wrapper;
@@ -17,6 +19,8 @@ describe('<BurgerBuilder />', () => {
 
   it('should render BuildControls when receiving ingredients', () => {
     wrapper.setState({ingredients: {salad: 0}});
+    expect(wrapper.find(Burger)).toHaveLength(1);
     expect(wrapper.find(BuildControls)).toHaveLength(1);
+    expect(wrapper.find(OrderSummary)).toHaveLength(1);
   });
 });
