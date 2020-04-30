3f3a7c35e549099f59400b7dc1e07b015fda9f9b
mfradcourt <mfradcourt@gmail.com>
2020-04-30 11:02:32 +0200

    Add Checkout page Add Orders page Add contact data page Add navigation for Orders & Checkout pages

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,12 +3,15 @@ import NavigationItem from './NavigationItem/NavigationItem';
 import classes from './NavigationItems.module.css';
 
 const navigationItems = () => (
-    <ul className={classes.NavigationItems}>
-        <NavigationItem
-            link="/" active>Burger Builder</NavigationItem>
-        <NavigationItem
-            link="/">Checkout</NavigationItem>
-    </ul>
+  <ul className={classes.NavigationItems}>
+    <NavigationItem
+      link="/"
+      exact>Burger Builder</NavigationItem>
+    <NavigationItem
+      link="/checkout">Checkout</NavigationItem>
+    <NavigationItem
+      link="/orders">Orders</NavigationItem>
+  </ul>
 );
 
 export default navigationItems;
